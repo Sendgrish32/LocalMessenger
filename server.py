c09e98db90d725a1954bdea99cb7cb915fcad301
Sendgrish <sendgrish@gmail.com>
2026-05-12 16:51:04 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 import asyncio, json, hashlib, os
 
 #IP адрес сервера: Для только своего пк(127.0.0.1), для всех(0.0.0.0)
-ip_addr = '0.0.0.0'
+ip_addr = '127.0.0.1'
 
-#НАСТРОЙКИ И БАЗА ДАННЫХ
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_FILE = os.path.join(BASE_DIR, "users.json")
-
-# Загружаем базу при старте
 if os.path.exists(DB_FILE):
     with open(DB_FILE, "r", encoding="utf-8") as f:
         try:
@@ -20,15 +17,11 @@ else:
 active_users = {}
 
 def save_db():
-    #ФУНКЦИЯ СОХРАНЕНИЯ ДАННЫХ ПОЛЬЗОВАТЕЛЕЙ
     with open(DB_FILE, "w", encoding="utf-8") as f:
         json.dump(users_db, f, indent=4)
 
 def get_hash(password: str):
-    #ХЕШИРОВАНИЕ
     return hashlib.sha256(password.encode()).hexdigest()
-
-
 
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
@@ -38,20 +31,16 @@ async def handle_client(reader, writer):
         data = await reader.read(1024)
         content = data.decode().strip()
 
-        # 1. ЗАПРОС НИКНЕЙМА
         writer.write("Введите ваш никнейм: ".encode())
         await writer.drain()
         nickname = (await reader.read(1024)).decode().strip()
 
-        # 2. ЗАПРОС ПАРОЛЯ
         writer.write("Введите пароль: ".encode())
         await writer.drain()
         password = (await reader.read(1024)).decode().strip()
         password_hash = get_hash(password)
 
-        # 3. ПРОВЕРКА АВТОРИЗАЦИИ / РЕГИСТРАЦИЯ
         if nickname in users_db:
-            # Если юзер есть
             if users_db[nickname]["password_hash"] != password_hash:
                 writer.write("ОШИБКА: Неверный пароль".encode())
                 await writer.drain()
@@ -59,9 +48,8 @@ async def handle_client(reader, writer):
                 return
             writer.write(f"С возвращением, {nickname}!\n".encode())
         else:
-            # Если юзера нет
             users_db[nickname] = {"password_hash": password_hash}
-            save_db() #СОХРАНЯЕМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
+            save_db() #Saving user data
             writer.write(f"Регистрация успешна! Добро пожаловать, {nickname}.\n".encode())
         await writer.drain()
 
@@ -71,8 +59,6 @@ async def handle_client(reader, writer):
             save_db()
             print(f"[СЕРВЕР] Ключ для {nickname} сохранен.")
 
-
-        # 4. ВХОД В ЧАТ
         active_users[nickname] = writer
         print(f"[СЕРВЕР] {nickname} ({addr}) вошел в чат.")
         
@@ -82,7 +68,6 @@ async def handle_client(reader, writer):
                 w.write(msg.encode())
                 await w.drain()
 
-        # 5. ЦИКЛ ОБМЕНА СООБЩЕНИЯМИ
         while True:
             data = await reader.read(1024)
             if not data:
@@ -94,7 +79,6 @@ async def handle_client(reader, writer):
 
             print(f"[{nickname}]: {message}")
 
-            # Рассылка всем активным
             final_msg = f"[{nickname}]: {message}"
             for user, w in list(active_users.items()):
                 if user != nickname:
@@ -102,18 +86,15 @@ async def handle_client(reader, writer):
                         w.write(f"{final_msg}".encode())
                         await w.drain()
                     except:
-                        # Если не удалось отправить
                         del active_users[user]
 
     except Exception as e:
         print(f"[ОШИБКА] Проблема с {addr}: {e}")
     finally:
-        # Убираем пользователя из списка активных при выходе
         if nickname and nickname in active_users:
             del active_users[nickname]
             print(f"[СЕРВЕР] {nickname} покинул чат.")
             
-            # Оповещаем остальных об уходе
             exit_msg = f"\n[СИСТЕМА] {nickname} покинул чат.\n "
             for user, w in active_users.items():
                 try:
